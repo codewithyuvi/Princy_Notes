334120ec6779ff613aba39413839cd1f63847cd7
codewithyuvi <yuvrajbansal30dec@gmail.com>
2025-10-19 12:26:17 +0530

    Add form handling and API integration to AddNewSubject component

--- src/adminPortalComponents/AddNewSubject.jsx
+++ src/adminPortalComponents/AddNewSubject.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function AddNewSubject(){
+    
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async(e) => {
+        e.preventDefault(); // prevents reloading
+        try{
+            const res = await axios.post("http://localhost:8000/api/v1/admin/createSubject", {
+                subjectName: subject,
+            });
+            setMessage(res.data.message)
+            setSubject("")
+        } catch(err){
+            console.error(err);
+            setMessage("Error addind subject")
+        }
+    }
     
     return (
         <div className="d-inline-flex">
@@ -8,10 +26,22 @@ function AddNewSubject(){
             <div className="">
                 <h1>Add New Subject</h1>
 
-                <div class=" mb-3">
-                    <input type="text" class="form-control mb-3" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
-                    <input class="btn btn-primary" type="submit" value="Submit"></input>
-                </div>
+                <form onSubmit={handleSubmit}>
+                    <div class=" mb-3">
+                        <input 
+                            type="text" 
+                            class="form-control mb-3" 
+                            placeholder="Enter Subject Name" 
+                            value={subject}    
+                            onChange={(e) => setSubject(e.target.value)}
+                            required
+                        />
+                        
+                        <input class="btn btn-primary" type="submit" value="Submit"/>
+                    </div>
+                </form>
+
+                {message && <p>{message}</p>}
             </div>
         </div>
     )
