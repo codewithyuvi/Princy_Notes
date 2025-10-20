33c01a9e805eeb3e72f48e32afd4789891ec71d2
codewithyuvi <yuvrajbansal30dec@gmail.com>
2025-10-20 12:00:35 +0530

    Implement file upload functionality with dynamic form handling in UploadPdf component

--- src/adminPortalComponents/UploadPdf.jsx
+++ src/adminPortalComponents/UploadPdf.jsx
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function UploadPdf() {
+  const [showNotesInput, setShowNotesInput] = useState(false);
+    const [message, setMessage] = useState("");
 
-  function checkType() {
-    console.log(document.getElementsByClassName("selectType")[0].value);
-    if (document.getElementsByClassName("selectType")[0].value === "notes") {
-        document.getElementsByClassName("notesInputs")[0].style.display = "block"
+  function checkType(e) {
+    const selected = e.target.value;
+    setShowNotesInput(selected === "notes");
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("fileName", e.target.fileName.value);
+    formData.append("fileType", e.target.type.value);
+    formData.append("fileUrl", e.target.pdf.files[0]);
+
+    if(e.target.type.value === "notes"){
+        formData.append("unitName", e.target.unitName.value);
+        formData.append("subjectId", e.target.subjectId.value);
     }
-    else{
-        document.getElementsByClassName("notesInputs")[0].style.display = "none"
+
+    try{
+        const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/admin/uploadFile`, 
+            formData,
+            {
+                headers: { "Content-Type": "multipart/form-data" }
+            }
+        )
+        setShowNotesInput(false);
+        console.log(res.data);
+        setMessage(e.target.fileName.value + " " + res.data.message);
+    } catch(err){
+        if(err.response && err.response.data){
+            setMessage(err.response.data.message || "Kismat Kharab")
+        }
+        else{
+            setMessage("err")
+        }
     }
   }
+
+
 
   return (
     <div className="d-inline-flex flex-column">
       <h1>Upload Pdf</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="type">Enter File Name</label>
           <input
             type="text"
+            name="fileName"
             className="form-control mb-3"
             placeholder="Enter File Name"
             // value={subject}
@@ -31,19 +65,23 @@ function UploadPdf() {
           <label htmlFor="type" className="d-flex">
             Select Type of Pdf
           </label>
-          <select name="type" className="selectType" onChange={checkType}>
-            <option value="">Please choose the Type of PDF</option>
-            <option value="notes">Notes</option>
-            <option value="pyq">PYQ</option>
-            <option value="syllabus">Syllabus</option>
-          </select>
 
-          <div className="notesInputs" style={{ display: "none" }}>
+          
+            <select name="type" className="selectType" onChange={checkType} required>
+              <option value="">Please choose the Type of PDF</option>
+              <option value="notes">Notes</option>
+              <option value="pyq">PYQ</option>
+              <option value="syllabus">Syllabus</option>
+            </select>
+        
+    {showNotesInput && (
+          <div className="notesInputs">
             <label htmlFor="type" className="d-flex">
               Enter Unit Name
             </label>
             <input
               type="text"
+              name="unitName"
               className="form-control mb-3"
               placeholder="Enter the Unit Name"
             />
@@ -58,11 +96,14 @@ function UploadPdf() {
               <option value=""></option>
             </select>
           </div>
-          <input type="file" accept=".pdf" className="d-flex" />
 
-          <input className="btn btn-primary" type="submit" value="Submit" />
+    )}
+          <input type="file" name="pdf" accept=".pdf" className="d-flex" />
+
+          <input className="btn btn-primary" type="submit" value="Submit" required/>
         </div>
       </form>
+      {message && <p>{message}</p>}
     </div>
   );
 }
