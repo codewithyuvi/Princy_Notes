1f4d4b4057f034e52ed2ec615cc7505bbcea51d6
codewithyuvi <yuvrajbansal30dec@gmail.com>
2025-10-22 00:54:25 +0530

    Refactor AllNotes component to include subject and unit fetching with error handling

--- src/adminPortalComponents/AllNotes.jsx
+++ src/adminPortalComponents/AllNotes.jsx
@@ -1,10 +1,103 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function AllNotes(){
     
+    const [subjects, getSubjects] = useState([]);
+    const [message, setMessage] = useState("");
+    const [selectedSubjectId, setSelectedSubjectId] = useState();
+    const [selectedSubjectName, setSelectedSubjectName] = useState("");
+
+    const [units, getUnits] = useState([]);
+
+
+    useEffect(() => {
+        fetchSubject();
+    }, []);
+
+    useEffect(() => {
+        fetchUnits();
+    });
+
+    async function fetchSubject(){
+        
+        try{
+
+            const queryParams = {
+                type: "notes"
+            }
+            
+            const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/admin/getType`, {params: queryParams})
+            getSubjects(res.data.data)
+            // console.log(res.data.data);
+            
+        } catch(err) {
+            if(err.response && err.response.data){
+                setMessage(err.response.data.message || "Something went")
+            }
+            else{
+                setMessage("error");
+            }
+        }
+    } 
+
+    async function selectedSubject(i){
+        setSelectedSubjectId(i._id)
+        setSelectedSubjectName(i.subjectName)
+    }
+
+    async function fetchUnits(){
+        try{
+            const queryParams = {
+                subjectId: selectedSubjectId
+            }
+            
+            const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/admin/subject`, {params: queryParams})
+            getUnits(res.data.data)
+            // console.log(res.data.data);
+        } catch(err){
+            if(err.response && err.response.data){
+                setMessage(err.response.data.message || "Something went")
+            }
+            else{
+                setMessage("error");
+            }
+        }
+    }
     return (
-        <div className="d-inline-flex">
-            <h1>i am notes</h1>
+        <div className="d-inline-flex flex-column">
+            <h1>Notes</h1>
+            {selectedSubjectId  ? 
+                <ol>All units for {selectedSubjectName} 
+                    {units.length > 0 ? (
+                    units.map((i, n) => (
+  <li key={n}>
+    <a href={i.fileUrl} target="_blank" rel="noopener noreferrer">
+      {i.unitName}
+    </a>
+  </li>
+))
+                ) : (
+                    <li>No Subject Found</li>
+                )}
+                
+                
+                </ol>
+            : (
+
+                <ol>
+                {subjects.length > 0 ? (
+                    subjects.map((i,n) => <li key={n} onClick={() => selectedSubject(i)} >{i.subjectName}</li>)
+                ) : (
+                    <li>No Subject Found</li>
+                )}
+            </ol>
+            )
+}
+
+            
+
+            {message && <p>{message}</p>}
         </div>
     )
 }
