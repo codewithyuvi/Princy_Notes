af184caad72ebc712b0eb60536387f4d6ceea56f
codewithyuvi <yuvrajbansal30dec@gmail.com>
2026-03-31 11:02:40 +0530

    temporary changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="footer-container">
       <div className="footer-content">
         <div className="footer-section about">
-          <h3>Princy Bansal</h3>
+          <h3>Edu Pro</h3>
           <p>
             Your trusted partner for easy access to PYQs, notes, and syllabus.  
             Study smart, score high!
@@ -45,16 +45,16 @@ const Footer = () => {
 
       <div className="footer-bottom">
         <p>
-          © 2025 Edu Pro. Made with ❤️ for students. <br />
-          Developed by{" "}
-          <a
+          © 2026 Edu Pro. Made with ❤️ for students. <br />
+          {/* Developed by{" "} */}
+          {/* <a
             href="https://www.linkedin.com/in/yuvrajbansal"
             target="_blank"
             rel="noreferrer"
             className="footer-credit-link"
           >
             Yuvraj Bansal
-          </a>
+          </a> */}
         </p>
       </div>
     </footer>
